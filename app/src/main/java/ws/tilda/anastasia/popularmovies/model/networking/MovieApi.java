1b0b150132f134ca86058b31c87a538165dc2740
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-03-31 19:08:40 +0300

    update

--- app/src/main/java/ws/tilda/anastasia/popularmovies/model/networking/MovieApi.java
+++ app/src/main/java/ws/tilda/anastasia/popularmovies/model/networking/MovieApi.java
@@ -10,8 +10,8 @@ import retrofit2.http.Path;
 import ws.tilda.anastasia.popularmovies.model.model_objects.Response;
 
 public class MovieApi {
-    private static final String APIKEY = "Put Your API Key";
-    private static final String APIPATH = " https://api.themoviedb.org/3/";
+    private static final String APIKEY = "putYourApiKey";
+    private static final String APIPATH = "https://api.themoviedb.org/3/";
     private static OkHttpClient.Builder okhttpClientBuilder = new OkHttpClient.Builder();
 
     private static void getLogger() {
@@ -38,7 +38,7 @@ public class MovieApi {
         Call<Response> getMovies();
 
         @GET("movie/{criteria}?api_key=" + APIKEY)
-        Call<Response> sortMoviesByCriteria(@Path ("criteria") String string);
+        Call<Response> sortMoviesByCriteria(@Path("criteria") String string);
     }
 
 }
