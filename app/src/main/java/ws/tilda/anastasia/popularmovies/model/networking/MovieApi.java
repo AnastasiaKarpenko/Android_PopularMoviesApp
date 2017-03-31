52dd2e4eacd15f8a6509f9044c5ce2987670a681
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-03-31 16:50:30 +0300

    added Okhttp logging interceptor

--- app/src/main/java/ws/tilda/anastasia/popularmovies/model/networking/MovieApi.java
+++ app/src/main/java/ws/tilda/anastasia/popularmovies/model/networking/MovieApi.java
@@ -1,5 +1,7 @@
 package ws.tilda.anastasia.popularmovies.model.networking;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -8,18 +10,26 @@ import retrofit2.http.Path;
 import ws.tilda.anastasia.popularmovies.model.model_objects.Response;
 
 public class MovieApi {
-    private static final String APIKEY = "PutYourApiKey";
+    private static final String APIKEY = "Put Your API Key";
     private static final String APIPATH = " https://api.themoviedb.org/3/";
+    private static OkHttpClient.Builder okhttpClientBuilder = new OkHttpClient.Builder();
 
+    private static void getLogger() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        okhttpClientBuilder.addInterceptor(logging);
+    }
 
     public static Retrofit provideRetrofit() {
         return new Retrofit.Builder()
                 .baseUrl(APIPATH)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okhttpClientBuilder.build())
                 .build();
     }
 
     public static MovieService provideMovieService() {
+        getLogger();
         return provideRetrofit().create(MovieService.class);
     }
 
